2d3abac4c4652c124b783a501904c57402223772
Im-adom <graceamaningkwarteng@gmail.com>
2023-04-27 06:12:53 -0700

    2. Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,14 +2,12 @@
 
 /**
  * mul- Multiplies two digits.
- *
- * @a- int to multiply
- * @b- int to multiply
- *
+ * @a: The first number to multiply.
+ * @b: The second number to multiply with first number.
+ * Return: The result of the multiplication between
+ * parameter  a and parameter b.
  */
 int mul(int a, int b)
 {
-	int c;
-	c = a * b;
-	return (void);
+	return (a * b);
 }
