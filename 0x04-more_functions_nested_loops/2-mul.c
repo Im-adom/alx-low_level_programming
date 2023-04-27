297c0ea5cace165f7da987b91b8ff7d0b8e7cbcc
Im-adom <graceamaningkwarteng@gmail.com>
2023-04-27 05:31:20 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * mul- Multiplies two digits.
+ *
+ * @a- int to multiply
+ * @b- int to multiply
+ *
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (void);
+}
