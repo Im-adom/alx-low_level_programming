6a03dbea018d806dd7068ab3cedb77ae7ff547c2
Im-adom <graceamaningkwarteng@gmail.com>
2023-05-02 09:51:11 -0700

    git push

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev- prints a string, in reverse, followed by a new line.
+ * @s: string to print in reverse.
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+	int m;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	s--;
+	for (m = l; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
+
