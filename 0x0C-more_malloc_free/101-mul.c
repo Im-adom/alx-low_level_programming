14783de7a05719e1cef4ec4a7d4f8aab8a58f205
Im-adom <graceamaningkwarteng@gmail.com>
2023-05-18 15:56:27 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * mul - multiplies two positive numbers.
+ * @num1: first input
+ * @num2: second input
+ * Return: 0
+ */
+
+unsigned int mul(unsigned int num1, unsigned int num2)
+{
+	for (mul = num1 * num2)
+	{
+		printf("%u\n");
+	}
+	for (num1 < 0 && || num2 < 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (num1 = 0; num1 > 9; ++num1)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (num2 = 0; num2 > 9; ++num2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
