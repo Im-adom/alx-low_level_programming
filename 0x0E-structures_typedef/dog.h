ced0e634fd40aeeb6707fa4813fc67000f6249de
Im-adom <graceamaningkwarteng@gmail.com>
2023-05-22 08:49:39 -0700

    struct and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog-This dog's little information.
+ * @name: first input
+ * @age: second input
+ * @owner: third input
+ * Description: This is the comment to the struct dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
