3c99db5ab7846eae372d1bf9339e098169bd4502
Im-adom <graceamaningkwarteng@gmail.com>
2023-07-21 16:19:09 -0700

    Updated

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,28 +8,21 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node = NULL;
 
-	if (!head)
+	if (head)
 	{
-		return (NULL);
-	}
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
+	new_node = malloc(sizeof(*new_node));
+	if (new_node)
 	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->n = n;
-	if (*head == NULL)
-	{
-		new_node->next = NULL;
+		new_node->n = n;
 		new_node->prev = NULL;
+		new_node->next = *head;
+
+		if (*head)
+			(*head)->prev = new_node;
 		*head = new_node;
-		return (new_node);
 	}
-	new_node->next = *head;
-	new_node->prev = NULL;
-	(*head)->prev = new_node;
+	}
 	return (new_node);
 }
